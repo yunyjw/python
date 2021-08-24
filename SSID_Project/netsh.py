fad51ffbb8a7cc2c79b0b7a91f602d5a29efb272
yunyjw <yunyjw95@naver.com>
2021-08-24 17:03:46 +0900

    ssid-3

--- SSID_Project/netsh.py
+++ SSID_Project/netsh.py
@@ -1,18 +1,52 @@
+# 숨겨진 ssid 자동 설정 exe 파일
+# 작업 순서 
 '''
-netsh wlan export profile name=daeduck_AGV key=clear folder=c:\Wi-Fi
-netsh wlan add profile filename="C:\Wi-Fi\Wi-Fi-daeduck_office.xml" Interface="Wi-Fi" user=current
-netsh wlan connect ssid=daeduck_office name=daeduck_office interface=Wi-Fi
-netsh wlan set profileparameter name=daeduck_office connectionmode=auto
+1. daeduck_office xml 파일을 사용자 C:\Wi-Fi 폴더를 만들어서 추가 
+2. daeduck_office ssid 프로필 추가
+3. daeduck_office ssid 접속
+4. daeduck_office ssid 자동 연결
 '''
 
 import os
-# import subprocess # subprocess는 파이썬에서 쉘 명령을 실행할 수 있게 해주는 라이브러리
+# daeduck_office xml 파일 내용
+'''
+import xml.etree.ElementTree as ET
 
-# ssid 프로파일 추출 명령어
-os.system('netsh wlan export profile name=daeduck_AGV key=clear folder=c:\Wi-Fi')
+xml = <?xml version="1.0"?>
+<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+	<name>daeduck_office</name>
+	<SSIDConfig>
+		<SSID>
+			<hex>6461656475636B5F414756</hex>
+			<name>daeduck_office</name>
+		</SSID>
+		<nonBroadcast>true</nonBroadcast>
+	</SSIDConfig>
+	<connectionType>ESS</connectionType>
+	<connectionMode>manual</connectionMode>
+	<MSM>
+		<security>
+			<authEncryption>
+				<authentication>WPA2PSK</authentication>
+				<encryption>AES</encryption>
+				<useOneX>false</useOneX>
+			</authEncryption>
+			<sharedKey>
+				<keyType>passPhrase</keyType>
+				<protected>false</protected>
+				<keyMaterial>daeduckwireless12345543210</keyMaterial>
+			</sharedKey>
+		</security>
+	</MSM>
+</WLANProfile>
+'''
+
+# ssid 프로파일 추출 명령어 ( XML 파일을 사용자 PC에 저장시키면 이 작업은 굳이 안해도 됨)
+os.system('netsh wlan export profile name=daeduck_office key=clear folder=c:\Wi-Fi')
 # 신규 프로파일 추가 명령어
-os.system('netsh wlan add profile filename="C:\Wi-Fi\Wi-Fi-daeduck_AGV.xml" Interface="Wi-Fi" user=current')
+os.system('netsh wlan add profile filename="C:\Wi-Fi\Wi-Fi-daeduck_office.xml" Interface="Wi-Fi" user=current')
 # 프로파일 이용하여 ssid 접속 명령어 ( Wi-Fi )
-os.system('netsh wlan connect ssid=daeduck_AGV name=daeduck_AGV interface=Wi-Fi')
+os.system('netsh wlan connect ssid=daeduck_AGV name=daeduck_office interface=Wi-Fi')
 # 추가 한 ssid에 자동 연결 시키는 명령어
-os.system('netsh wlan set profileparameter name=daeduck_AGV connectionmode=auto')
+os.system('netsh wlan set profileparameter name=daeduck_office connectionmode=auto')
+
