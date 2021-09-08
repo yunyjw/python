bf10160c427bb2f292a3a775a0032e5b84673753
yunyjw <yunyjw95@naver.com>
2021-09-09 00:46:11 +0900

    210909-input

---
+++ 챕터/chapter5-2.py
@@ -0,0 +1,45 @@
+# chapter5-2
+# 파이썬 사용자 입력
+# Input 사용법 ( runtime 때 입력을 받겟다 )
+# 기본 타입(str)
+
+# 예제 1
+'''
+name = input("Enter Your Name : ")
+grade = input("Enter Your Grade : ")
+company = input("Enter Your Company name : ")
+
+print(name, grade, company)
+
+# cmd 진입해서 cd D:\Github\python_study\챕터\python_study\챕터 경로 이동 후 해당 py 파일 실행
+'''
+
+# 예제 2
+'''
+number = input("Enter number : ")
+name = input("Enter name : ")
+
+print("type of number", type(number))
+print("type of name", type(name))
+
+# type은 기본 str 이기 떄문에 사칙연산 시 문자가 반복되어 나옴
+'''
+
+# 예제 3
+#first_number = int(input("Enter number1 : "))
+#second_number = int(input("Enter number2 : "))
+#
+#total = first_number + second_number
+#print("first_number + second_number : ", total)
+# 형 변환을 하여 int 계산
+
+# 예제 4
+#float_number = float(input("Enter Float number : "))
+
+#print("input float : ", float_number * 21.11)
+#print("input type : ", type(float_number))
+
+# 예제 5
+# print("FirstName -{0}, LastName - {1}".format(input("Enter first name : "), input("Enter second name : ")))
+
+# idel에서는 input 바로 테스트 가능 ( 파이썬 쉘 )
