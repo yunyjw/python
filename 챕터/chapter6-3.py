35e10d985c76b9cd41e894e2f7a2deb82700569e
yunyjw <yunyjw95@naver.com>
2021-09-19 16:38:27 +0900

    210919

---
+++ 챕터/chapter6-3.py
@@ -0,0 +1,26 @@
+# chapter6-3
+# 파이썬 패키지
+# 패키지 작성 및 사용법
+# 파이썬은 패키지로 분할 된 개별적인 모듈로 구성
+# 상대경로 : .. (부모 디렉토리), .(현재 디렉토리) -> 모듈 내부에서만 사용
+
+# 예제1
+import sub.sub1.module1
+import sub.sub2.module2
+
+# 사용
+sub.sub1.module1.mod1_test1()
+sub.sub1.module1.mod1_test2()
+
+sub.sub2.module2.mod2_test1()
+sub.sub2.module2.mod2_test2()
+
+print()
+print()
+print()
+
+# 예제2
+from sub.sub1 import module1
+
+
+
---
+++ 챕터/module_test.py
@@ -0,0 +1,20 @@
+# 모듈 사용 실습
+
+import sys 
+
+print(sys.path)
+
+print(type(sys.path))
+
+# 모듈 경로 삽입
+sys.path.append('C:\\math')
+# sys.path.append('C:\\Git\\python_study\\챕터')
+print(sys.path)
+# import test_module
+import chapter
+
+ 
+# 모듈 사용
+# print(test_module.power(10, 3))
+
+# import chapter6-2
